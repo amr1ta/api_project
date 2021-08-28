2ec57436d044753c526894e0abd9b6da9a18a27b
Amrita Mahapatra <amrita.mahapatra11@gmail.com>
2021-08-28 23:27:35 +0530

    pass parameters to test function

--- day2/test_api_methods.py
+++ day2/test_api_methods.py
@@ -1,5 +1,6 @@
 import api_client
 import json
+import pytest
 
 apiclient = api_client.Api_Client(url="http://localhost:3000/users")
 
@@ -9,16 +10,22 @@ class Test_Users_Api:
         status_code, _ = apiclient.get_all_users()
         assert status_code == 200
 
-    def test_get_user_by_id(self):
-        status_code, _ = apiclient.get_user_by_id(3)
+    @pytest.mark.parametrize("user_id", [1, 3, 4])
+    def test_get_valid_user_by_id(self, user_id):
+        status_code, _ = apiclient.get_user_by_id(user_id)
         assert status_code == 200
+
+    @pytest.mark.parametrize("user_id", [20, 30, 40])
+    def test_get_invalid_user_by_id(self, user_id):
+        status_code, _ = apiclient.get_user_by_id(user_id)
+        assert status_code == 404
 
     def test_create_user(self):
         with open("post_payload.json") as f:
             user_payload = json.load(f)
         status_code, response = apiclient.create_user(user_payload)
         assert status_code == 201
-        Test_Users_Api._created_user_id = response['id']
+        Test_Users_Api._created_user_id = response["id"]
 
     def test_delete_user(self):
         status_code = apiclient.delete_user(Test_Users_Api._created_user_id)
